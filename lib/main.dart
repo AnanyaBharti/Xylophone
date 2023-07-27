b67009c3ab01717868cd1c940af83280c85ac92b
Ananya Bharti <ananyabharti@Ananyas-MacBook-Air.local>
2023-07-27 15:19:14 +0530

    Added Gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(const XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
   // Future<void> playSound(int SoundNumber) async {
   //   final player = AudioPlayer();
   //   await player.play(
@@ -15,93 +19,87 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            children: [
-              MusicTile(number: 1, color: Colors.purple),
-              MusicTile(number: 2, color: Colors.lime),
+        backgroundColor: Colors.white,
+        body: Column(
+          children: const [
+            MusicTile(number: 1, color: Colors.purple),
+            MusicTile(number: 2, color: Color(0xFFB56611)),
+            MusicTile(number: 3, color: Colors.blue),
+            MusicTile(number: 4, color: Color(0xFFC8DD0F)),
+            MusicTile(number: 5, color: Colors.green),
+            MusicTile(number: 6, color: Colors.red),
+            MusicTile(number: 7, color: Colors.pink),
 
-              MusicTile(number: 3, color: Colors.blue),
-
-              MusicTile(number: 4, color: Colors.yellow),
-
-              MusicTile(number: 5, color: Colors.green),
-
-              MusicTile(number: 6, color: Colors.red),
-              MusicTile(number: 7, color: Colors.pink),
-
-              // TextButton(
-              //   onPressed: () async {
-              //     playSound(1);
-              //   },
-              //   child: Container(
-              //     // height: 50,
-              //     width: double.infinity,
-              //     color: Colors.purple,
-              //   ),
-              // ),
-              // TextButton(
-              //   onPressed: () async {
-              //     playSound(2);
-              //   },
-              //   child: Container(
-              //     // height: 50,
-              //     width: double.infinity,
-              //     color: Colors.blue.shade900,
-              //   ),
-              // ),
-              // Expanded(
-              //   child: TextButton(
-              //     onPressed: () async {
-              //       playSound(3);
-              //     },
-              //     child: Container(
-              //       // height: 50,
-              //       width: double.infinity,
-              //       color: Colors.yellow,
-              //     ),
-              //   ),
-              // ),
-              // MusicTile(),
-              // Expanded(
-              //   child: TextButton(
-              //     onPressed: () async {
-              //       playSound(5);
-              //     },
-              //     child: Container(
-              //       // height: 50,
-              //       width: double.infinity,
-              //       color: Colors.orange,
-              //     ),
-              //   ),
-              // ),
-              // Expanded(
-              //   child: TextButton(
-              //     onPressed: () async {
-              //       playSound(6);
-              //     },
-              //     child: Container(
-              //       // height: 50,
-              //       width: double.infinity,
-              //       color: Colors.red,
-              //     ),
-              //   ),
-              // ),
-              // Expanded(
-              //   child: TextButton(
-              //     onPressed: () async {
-              //       playSound(7);
-              //     },
-              //     child: Container(
-              //       // height: 50,
-              //       width: double.infinity,
-              //       color: Colors.pink,
-              //     ),
-              //   ),
-              // ),
-            ],
-          ),
+            // TextButton(
+            //   onPressed: () async {
+            //     playSound(1);
+            //   },
+            //   child: Container(
+            //     // height: 50,
+            //     width: double.infinity,
+            //     color: Colors.purple,
+            //   ),
+            // ),
+            // TextButton(
+            //   onPressed: () async {
+            //     playSound(2);
+            //   },
+            //   child: Container(
+            //     // height: 50,
+            //     width: double.infinity,
+            //     color: Colors.blue.shade900,
+            //   ),
+            // ),
+            // Expanded(
+            //   child: TextButton(
+            //     onPressed: () async {
+            //       playSound(3);
+            //     },
+            //     child: Container(
+            //       // height: 50,
+            //       width: double.infinity,
+            //       color: Colors.yellow,
+            //     ),
+            //   ),
+            // ),
+            // MusicTile(),
+            // Expanded(
+            //   child: TextButton(
+            //     onPressed: () async {
+            //       playSound(5);
+            //     },
+            //     child: Container(
+            //       // height: 50,
+            //       width: double.infinity,
+            //       color: Colors.orange,
+            //     ),
+            //   ),
+            // ),
+            // Expanded(
+            //   child: TextButton(
+            //     onPressed: () async {
+            //       playSound(6);
+            //     },
+            //     child: Container(
+            //       // height: 50,
+            //       width: double.infinity,
+            //       color: Colors.red,
+            //     ),
+            //   ),
+            // ),
+            // Expanded(
+            //   child: TextButton(
+            //     onPressed: () async {
+            //       playSound(7);
+            //     },
+            //     child: Container(
+            //       // height: 50,
+            //       width: double.infinity,
+            //       color: Colors.pink,
+            //     ),
+            //   ),
+            // ),
+          ],
         ),
       ),
     );
@@ -111,16 +109,10 @@ class XylophoneApp extends StatelessWidget {
 class MusicTile extends StatelessWidget {
   final int number;
   final Color color;
-  const MusicTile({
-    super.key,
-    required this.number,
-    required this.color,
-  });
+  const MusicTile({super.key, required this.number, required this.color});
   Future<void> playSound(int soundNumber) async {
     final player = AudioPlayer();
-    await player.play(
-      AssetSource('sounds/note$soundNumber.wav'),
-    );
+    await player.play(AssetSource('sounds/note$soundNumber.wav'));
   }
 
   @override
@@ -128,13 +120,16 @@ class MusicTile extends StatelessWidget {
     return Expanded(
       child: TextButton(
         style: TextButton.styleFrom(padding: EdgeInsets.zero),
-        onPressed: () async {
-          playSound(number);
-        },
+        onPressed: () async => playSound(number),
         child: Container(
-          // height: MediaQuery.of(context).size.height / 7,
           width: double.infinity,
-          color: color,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [Colors.white, color],
+            ),
+          ),
         ),
       ),
     );
